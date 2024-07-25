8c9e2773eb74048c27a4e0935cd7beb0cbe2a34c
Qyea <kreshess@mail.ru>
2024-07-25 23:05:29 +0300

    Initial Commit of Ozis res

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, ProfileCardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'ozi';
+}
