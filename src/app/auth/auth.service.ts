83dd21d062578ca17ba9f5586ae6b9c8e98f8ccd
Qyea <kreshess@mail.ru>
2024-07-29 23:56:03 +0300

    Auth guard

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+import { TokenResponse } from './auth.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,12 @@ export class AuthService {
 
   baseApiUrl = 'https://icherniakov.ru/yt-course/auth/'
 
+  token: string | null = null
+  refreshToken: string | null = null
+
+  get isAuth(){
+    return !!this.token
+  }
 
   login(payload: {username:string, password: string}){
     const formData = new FormData()
@@ -16,6 +24,11 @@ export class AuthService {
     formData.append('username', payload.username)
     formData.append('password', payload.password)
 
-    return this.http.post(`${this.baseApiUrl}token`, formData)
+    return this.http.post<TokenResponse>(`${this.baseApiUrl}token`, formData).pipe(
+      tap(val=>{
+        this.token = val.access_token
+        this.refreshToken = val.refresh_token
+      })
+    )
   }
 }
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../auth/auth.service';
+import { from } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -11,6 +13,7 @@ import { AuthService } from '../../auth/auth.service';
 })
 export class LoginPageComponent {
   authService = inject(AuthService)
+  router = inject(Router)
 
   form = new FormGroup({
     username: new FormControl(null, Validators.required),
@@ -21,6 +24,7 @@ export class LoginPageComponent {
     if(this.form.valid){
       //@ts-ignore
       this.authService.login(this.form.value).subscribe(res =>{
+        this.router.navigate([''])
         console.log(res)
       })
     }
