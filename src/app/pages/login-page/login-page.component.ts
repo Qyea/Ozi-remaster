bd47938d7d52b303a065925ba5859e6bc5be5026
Qyea <kreshess@mail.ru>
2024-08-01 00:03:26 +0300

    Signals with binding

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -19,6 +19,9 @@ import { Router } from '@angular/router';
 export class LoginPageComponent {
   authService = inject(AuthService);
   router = inject(Router);
+
+  isPasswordVisible = signal<boolean>(false)
+
 
   form = new FormGroup({
     username: new FormControl(null, Validators.required),
