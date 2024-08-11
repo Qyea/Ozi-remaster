7bd02edb282b7ca8f85bf83bed2efe598246b389
Qyea <kreshess@mail.ru>
2024-08-11 11:31:14 +0300

    post input + navigation on search

--- src/app/pages/profile-page/post-input/post-input.component.ts
+++ src/app/pages/profile-page/post-input/post-input.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ImgUrlPipe } from '../../../helpers/pipes/img-url.pipe';
+import { ProfileService } from '../../../data/services/profile.service';
 
 @Component({
   selector: 'app-post-input',
   standalone: true,
-  imports: [],
+  imports: [ImgUrlPipe],
   templateUrl: './post-input.component.html',
   styleUrl: './post-input.component.scss'
 })
 export class PostInputComponent {
+  profileService = inject(ProfileService)
 
+  me = this.profileService.me
 }
--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -4,17 +4,20 @@ import { ProfileService } from '../../data/services/profile.service';
 import { ProfileCardComponent } from '../../common-ui/profile-card/profile-card.component';
 import { ProfileFiltersComponent } from './profile-filters/profile-filters.component';
 import {AsyncPipe} from "@angular/common";
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-search-page',
   standalone: true,
-  imports: [ProfileCardComponent, ProfileFiltersComponent, AsyncPipe],
+  imports: [ProfileCardComponent, ProfileFiltersComponent, AsyncPipe, RouterLink],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss',
 })
 export class SearchPageComponent {
   profileService = inject(ProfileService);
   profiles = this.profileService.filteredProfiles
+
+  route = inject(ActivatedRoute)
 
   constructor() {
     
