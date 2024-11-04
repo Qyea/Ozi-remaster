1d870a9acc389445edc296348c863c4c968ccd4c
Qyea <kreshess@mail.ru>
2024-11-04 23:38:50 +0300

    Minor fixes

--- src/app/pages/settings-page/settings-page.component.ts
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -1,16 +1,17 @@
-import { Component, effect, inject, ViewChild } from '@angular/core';
-import { ProfileHeaderComponent } from "../../common-ui/profile-header/profile-header.component";
-import { FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-import { ProfileService } from '../../data/services/profile.service';
-import { firstValueFrom } from 'rxjs';
-import { AvatarUploadComponent } from "./avatar-upload/avatar-upload.component";
+import { Component, effect, inject, ViewChild } from '@angular/core'
+import { ProfileHeaderComponent } from '../../common-ui/profile-header/profile-header.component'
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms'
+import { ProfileService } from '../../data/services/profile.service'
+import { firstValueFrom } from 'rxjs'
+import { AvatarUploadComponent } from './avatar-upload/avatar-upload.component'
+import { Profile } from '../../data/interfaces/profile.interface'
 
 @Component({
   selector: 'app-settings-page',
   standalone: true,
   imports: [ProfileHeaderComponent, ReactiveFormsModule, AvatarUploadComponent],
   templateUrl: './settings-page.component.html',
-  styleUrl: './settings-page.component.scss'
+  styleUrl: './settings-page.component.scss',
 })
 export class SettingsPageComponent {
   fb = inject(FormBuilder)
@@ -21,53 +22,64 @@ export class SettingsPageComponent {
   form = this.fb.group({
     firstName: ['', Validators.required],
     lastName: ['', Validators.required],
-    username: [{value:'', disabled: true}, Validators.required],
-    description:[''],
-    stack:['']
+    username: [{ value: '', disabled: true }, Validators.required],
+    description: [''],
+    stack: [''],
   })
 
-  constructor(){
-    effect(()=>{
-      //@ts-ignore
+  constructor() {
+    effect(() => {
       this.form.patchValue({
         ...this.profileService.me(),
-        //@ts-ignore
-        stack: this.mergeStack(this.profileService.me()?.stack)
-      }) 
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-expect-error
+        stack: this.mergeStack(this.profileService.me()?.stack),
+      })
     })
   }
 
-  onSave(){
+  onSave() {
     this.form.markAllAsTouched()
     this.form.updateValueAndValidity()
 
-    if(this.form.invalid) {
+    if (this.form.invalid) {
       console.log('Invalid')
       return
     }
 
-    if(this.avatarUploader.avatar){
-      firstValueFrom(this.profileService.uploadAvatar(this.avatarUploader.avatar)) 
+    const formValue = {
+      ...this.form.value,
+      stack: this.splitStack(this.form.value.stack),
     }
 
-    //@ts-ignore
-    firstValueFrom(this.profileService.patchProfile({
-        ...this.form.value,
-        stack: this.splitStack(this.form.value.stack)
-      }
-    )) 
+    const sanitizedFormValue = Object.fromEntries(
+      Object.entries(formValue).map(([key, value]) => [
+        key,
+        value === null ? undefined : value,
+      ])
+    )
+
+    if (this.avatarUploader.avatar) {
+      firstValueFrom(
+        this.profileService.uploadAvatar(this.avatarUploader.avatar)
+      )
+    }
+
+    firstValueFrom(
+      this.profileService.patchProfile(sanitizedFormValue as Partial<Profile>)
+    )
   }
 
-  splitStack(stack: string | null | string[] | undefined): string[]{
-    if(!stack) return []
-    if(Array.isArray(stack))return stack
+  splitStack(stack: string | null | string[] | undefined): string[] {
+    if (!stack) return []
+    if (Array.isArray(stack)) return stack
 
     return stack.split(',')
   }
 
-  mergeStack(stack: string | null | string[] | undefined){
-    if(!stack) return []
-    if(Array.isArray(stack))return stack.join(',')
+  mergeStack(stack: string | null | string[] | undefined) {
+    if (!stack) return []
+    if (Array.isArray(stack)) return stack.join(',')
 
     return stack
   }
